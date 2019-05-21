bc7ed1e821c5357bb54a453cff0835f824409ca0
stesta03 <samantha.testa@tufts.edu>
2019-05-21 17:26:13 -0400

    almost finished with filterByQuery, have to check description for query

--- client/src/utils/filter/filterByQuery.js
+++ client/src/utils/filter/filterByQuery.js
@@ -6,18 +6,28 @@
  * @return {object[]} apps that match the query
  */
 export default (apps, query) => {
-  // If query empty/whitespace, return all apps -> check first
-  // If query all apps, return all apps
-  if (query === null or query === '') {
+  // If query null/empty/only whitespace, return all apps
+  if (query === null || query === '' || query.match(/^\s+$/) !== null) {
     return apps;
   }
   // Make a new list to add the apps we want to return
-
+    const goodApps = new Array;
+    let length = 0;
   // Go through each app in the list
   // If the title, subtitle, or description (if it exists) match CASE
   // INSENSITIVE, add app to new list
+  query = query.toLowerCase();
+  for (let i = 0; i < apps.length; i++) {
+    // If query matches CASE INSENSITIVE, add and increment length
+    if (apps[i].title.toLowerCase() === query || apps[i].subtitle.toLowerCase()
+    === query) {
+      goodApps[length] = apps[i];
+      length += 1;
+    }
+    else if (apps[i].description.toLowerCase().search(/query/i)) {
+      // Look through description for query string
+      // TODO: look into methods for searching string for string
+    }
 
-
-  // Otherwise, move on
-
+  }
 };
