f0e1a9cbc86b76637e7333f9a5644cd7253ed6d7
stesta03 <samantha.testa@tufts.edu>
2019-05-23 20:49:39 -0400

    fixing test for filterByTags

--- test/client/src/utils/filter/filterByTags.spec.js
+++ test/client/src/utils/filter/filterByTags.spec.js
@@ -4,20 +4,30 @@ const filterByTags = require('../../../../../client/src/utils/filter/filterByTag
 
 describe('client > src > utils > filter > filterByTags', function () {
   const appTest = [
-   { tag: {
-       tagname => {
-         color: 'blue',
-         items: (itemName) => { return true; },
-      }
-    }
+    { name: 'AppOne',
+      tags: {
+        tagName => {
+          color: 'blue',
+          items: itemName => true;
+        }
+      }},
+    { name: 'AppTwo',
+      tags: {
+        tagName => {
+          color: 'red',
+          items: itemName => false;
+        }
+      }},
+    { name: 'AppThree',
+      tags: {
+        tagName => {
+          color: 'green',
+          items: itemName => true;
+        }
+      }},
   ];
 
-  const tags = {
-    (tagName) => {
-      color: 'red',
-      items: (itemName) => return true;
-    }
-  };
+
 
 
   it('Returns a list of apps that have a given tag name checked', async function () {
