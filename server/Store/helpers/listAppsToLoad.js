3aebc91c7e3af5cdc41dbd932a028b7cd8e81b7a
Shihan Li <lshhenry98@gmail.com>
2019-05-26 17:17:17 -0400

    added more descriptive variable name

--- server/Store/helpers/listAppsToLoad.js
+++ server/Store/helpers/listAppsToLoad.js
@@ -12,17 +12,17 @@ const STORE_PATH = require('../STORE_PATH');
  */
 module.exports = async () => {
   // create return object
-  const obj = {};
+  const appsToLoad = {};
   // list all catalogs
   const catalogIds = await listFolders(STORE_PATH);
   // populate object
   for (let i = 0; i < catalogIds.length; i++) {
-    obj[catalogIds[i]] = {};
+    appsToLoad[catalogIds[i]] = {};
     const apps = await listFolders(path.join(STORE_PATH, catalogIds[i]));
     for (let j = 0; j < apps.length; j++) {
       const appPath = path.join(STORE_PATH, catalogIds[i], apps[j]);
-      obj[catalogIds[i]][apps[j]] = appPath;
+      appsToLoad[catalogIds[i]][apps[j]] = appPath;
     }
   }
-  return obj;
+  return appsToLoad;
 };
