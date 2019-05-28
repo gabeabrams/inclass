b01e1057c96a1d155faa6fb12639b13176ac9114
stesta03 <samantha.testa@tufts.edu>
2019-05-28 16:56:02 -0400

    skeleton for genTagValueCounts.js

--- client/src/utils/filter/genTagValueCounts.js
+++ client/src/utils/filter/genTagValueCounts.js
@@ -11,6 +11,18 @@ const excludeTagName = require('./excludeTagName');
  *   the number of apps that have that tagItem while all the other tagNames are
  *   applied as filters
  */
-module.exports = (tags) => {
+module.exports = (apps, tags) => {
   // TODO: implement
+
+  // We need to go through each other filter category
+
+  // Go through each tag in filter cat, if it is checked, look for apps with
+  // that tag (filterByTags)
+  // FOR EACH app, add it to a new app array (check for dupes) and increment
+  // the count
+
+  // Then, we need to go through for each tag in THIS filter category, count
+  // how many apps from previous step have this current tag
+
+  // Put this number in this filter tag's count.
 };
---
+++ test/src/utils/filter/genTagValueCount.spec.js
@@ -0,0 +1,2 @@
+// Test for genTagValueCount.spec.js
+// 
