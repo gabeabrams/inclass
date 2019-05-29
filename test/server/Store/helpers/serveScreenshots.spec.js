9299cc247f677813e4bf6182d0f924a52473e023
Anita Lam <anitalam@Anitas-MacBook-Pro.local>
2019-05-29 11:10:40 -0400

    implemented test functions

--- test/server/Store/helpers/serveScreenshots.spec.js
+++ test/server/Store/helpers/serveScreenshots.spec.js
@@ -14,16 +14,34 @@ describe('server > Store > helpers > serveScreenshots', function () {
     assert.deepEqual(opts.app, Object(app)); // Check if correct syntax
   });
 
+  // Need to ask Henry if he can add this in
+  // How would this case occur?
   it('throws error for non-existing file', async function () {
 
   });
 
   it('correct url added to all screenshots\' property', async function () {
+    const opts = {
+      expressApp: express,
+      catalogId: 'dce',
+      appId: 'swipein',
+      app: '', // TODO
+    };
 
+    const app = await serveScreenshots(opts);
+    assert(app.screenshots[0].url === '/public/dce/swipein/screenshots/event_chooser');
+    assert(app.screenshots[1].url === '/public/dce/swipein/screenshots/man_dash');
   });
 
   it('returns updated app if there are screenshots', async function () {
-
+    const opts = {
+      expressApp: express,
+      catalogId: 'dce',
+      appId: 'swipein',
+      app: '', // TODO
+    };
+    const app = await serveScreenshots(opts);
+    assert(Object.prototype.hasOwnProperty.call(app.screenshots[0], 'url'));
   });
 
 
