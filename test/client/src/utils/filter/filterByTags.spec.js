85f03547cedf720d754d40e338cb199a5ec8e0d6
stesta03 <samantha.testa@tufts.edu>
2019-05-31 17:24:34 -0400

    added tests to filterByTags

--- test/client/src/utils/filter/filterByTags.spec.js
+++ test/client/src/utils/filter/filterByTags.spec.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const filterByTags = require('../../../../../client/src/utils/filter/filterByTags.js');
 
 describe.only('client > src > utils > filter > filterByTags', function () {
-  const testTags = {
+  const testTagsExpensiveAllTypes = {
     cost: {
       color: 'blue',
       tagValues: {
@@ -53,10 +53,10 @@ describe.only('client > src > utils > filter > filterByTags', function () {
   const testAppList = [AppOne, AppTwo, AppThree, AppFour];
 
   it('Returns a list of apps that have a given tag name checked', async function () {
-    assert.deepEqual(filterByTags(testAppList, testTags), [AppTwo, AppFour]);
+    assert.deepEqual(filterByTags(testAppList, testTagsExpensiveAllTypes), [AppTwo, AppFour], 'Returned applist is not equal to the expected apps');
   });
 
   it('Will not add an app if it doesn\'t have at least one item checked in each tag type', async function () {
-    assert.equal(filterByTags(testAppList, testTags)[0].name, 'AppTwo');
+    assert.deepEqual(filterByTags(testAppList, testTags)[0].name, 'AppTwo');
   });
 });
