027ba2b107e3a911f6b946ad6b3f79ae999c7f6f
stesta03 <samantha.testa@tufts.edu>
2019-05-31 12:40:43 -0400

    added comments

--- client/src/utils/filter/excludeTagName.js
+++ client/src/utils/filter/excludeTagName.js
@@ -12,7 +12,7 @@ module.exports = (tags, tagNameToExclude) => {
   // Makes a deep clone of tags without changing original
   const newTags = clone(tags);
 
-  
+  // Delete tagName from newTags
   delete newTags[tagNameToExclude];
 
   return newTags;
