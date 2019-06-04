58f07fed577e0337e84a7634448c9d0e347ce904
Gabe Abrams <gabeabrams@gmail.com>
2019-06-04 14:01:22 -0400

    Added shell for postProcessTags

---
+++ server/Store/helpers/postProcessTags.js
@@ -0,0 +1,11 @@
+/**
+ * Post-processes the tags within a catalog and its apps
+ * @module server/Store/helpers/postProcessTags
+ * @param {Catalog} catalog - a fully loaded catalog that includes its apps
+ * @return {Catalog} the updated catalog with a tagColors object and apps that
+ *   have at least one tagItem for every tagName (adds "other/uncategorized"
+ *   if no tagItems are in the app)
+ */
+module.exports = async (catalog) => {
+  // TODO: implement
+};
