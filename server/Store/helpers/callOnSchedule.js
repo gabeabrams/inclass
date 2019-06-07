1172c7865f487467bcf0cd9801461321807a9fe2
Shihan Li <lshhenry98@gmail.com>
2019-06-07 09:59:56 -0400

    implemented sinon for testing callOnSchedule

--- server/Store/helpers/callOnSchedule.js
+++ server/Store/helpers/callOnSchedule.js
@@ -1,19 +1,13 @@
 /**
- * Takes a function and calls it every x seconds
- * @param {function} func - the function to call x seconds
- * @param {number} [delay=60] - number of seconds to wait between calls
- * @return {function} kill function that stops the recurring calls
+ * Takes a function and calls it every x miliseconds
+ * @param {function} func - the function to call x miliseconds
+ * @param {number} [delay=500] - number of miliseconds to wait between calls
+ * @return {Interval Id} used to kill the timer and stop repeating call
  */
-module.exports = (func, delay = 60) => {
-  const delayInMiliseconds = delay * 1000;
-  setInterval(func, delayInMiliseconds);
-  // TODO: call func every minute from here until kill is called
-  const handle = () => {
-    setInterval(func, delayInMiliseconds);
-  };
+module.exports = async (func, delay = 500) => {
+  const refreshIntervalId = setInterval(func, delay);
   const kill = () => {
-    clearInterval(handle);
+    clearInterval(refreshIntervalId);
   };
   return kill;
 };
-// if function returns a working store, update it, save this as last working version of store
--- test/server/Store/helpers/callOnSchedule.spec.js
+++ test/server/Store/helpers/callOnSchedule.spec.js
@@ -1,20 +1,80 @@
+const assert = require('assert');
+const sinon = require('sinon');
 const callOnSchedule = require('../../../../server/Store/helpers/callOnSchedule');
 
-describe('server > Store > helpers > callOnSchedule', function () {
-  it('calls function multiple times', async function () {
-    const print = () => {
-      console.log('hello');
-    };
-    callOnSchedule(print, 2);
-  });
-});
+const aFuncShouldBeRunned = sinon.spy();
 
-/*
-* counter = 0
-* const func = () => { counter += 1 }
-* const kill = callOnSchedule(func, 200)
-*/
-// wait 300ms and check if the function was called once
-// wait untill 500 ms and check if the function was called twice
-// throw an error if not
-// kil
+describe.only('testing scheduled function calls', function () {
+  before(function () {
+    this.clock = sinon.useFakeTimers();
+  });
+
+  after(function () {
+    this.clock.restore();
+  });
+
+  it('calls function on time', function () {
+    let counter = 1;
+    const kill = callOnSchedule(() => {
+      if (counter === 3) {
+        aFuncShouldBeRunned();
+      }
+      if (counter === 5) {
+        aFuncShouldBeRunned();
+      }
+      counter += 1;
+      console.log(counter);
+    }, 500);
+    // At time 0, we don't expect the function to have been called.
+    assert(aFuncShouldBeRunned.called === false, 'function was called before timer started');
+    // Advance clock 500ms.
+    this.clock.tick(500);
+    assert(aFuncShouldBeRunned.called === false, 'function was called prematurely');
+
+    // Advance clock again (1s since start)
+    this.clock.tick(500);
+    assert(aFuncShouldBeRunned.called === false, 'function was called prematurely');
+
+    // Advance clock again (1.5s since start). This should
+    // trigger the call to `aFuncShouldBeRunned`.
+    this.clock.tick(500);
+    assert(aFuncShouldBeRunned.called === true, 'function was not called on schedule');
+    this.clock.tick(1000);
+    assert(aFuncShouldBeRunned.calledTwice, 'function was not called on schedule');
+    // kill();
+  });
+
+  // it('stops the recurring call when killed', function () {
+  //   before();
+  //   console.log(this.clock);
+  //   let counter = 1;
+  //   const kill = callOnSchedule(() => {
+  //     if (counter === 3) {
+  //       aFuncShouldBeRunned();
+  //     }
+  //     if (counter === 5) {
+  //       aFuncShouldBeRunned();
+  //     }
+  //     counter += 1;
+  //     console.log(counter);
+  //   }, 500);
+  //   // At time 0, we don't expect the function to have been called.
+  //   assert(aFuncShouldBeRunned.called === false, 'function was called before timer started');
+  //   // Advance clock 500ms.
+  //   this.clock.tick(500);
+  //   assert(aFuncShouldBeRunned.called === false, 'function was called prematurely');
+
+  //   // Advance clock again (1s since start)
+  //   this.clock.tick(500);
+  //   assert(aFuncShouldBeRunned.called === false, 'function was called prematurely');
+
+  //   // Advance clock again (1.5s since start). This should
+  //   // trigger the call to `aFuncShouldBeRunned`.
+  //   this.clock.tick(500);
+  //   assert(aFuncShouldBeRunned.called === true, 'function was not called on schedule');
+  //   this.clock.tick(1000);
+  //   assert(aFuncShouldBeRunned.calledTwice, 'function was not called on schedule');
+  //   // kill();
+  //   after();
+  // });
+});
