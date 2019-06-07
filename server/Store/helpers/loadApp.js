0bb6b02c609ef4f2634e11f0e39306b9b1eca65a
Gabe Abrams <gabeabrams@gmail.com>
2019-06-07 10:38:26 -0400

    Syntax cleaned up

--- server/Store/helpers/loadApp.js
+++ server/Store/helpers/loadApp.js
@@ -55,14 +55,12 @@ module.exports = async (opts = {}) => {
       }
     });
   }
+
   // if creator is not an array, turn it into one
-  if (appMetadata.creator) {
-    let creatorValue = appMetadata.creator;
-    if (!Array.isArray(creatorValue)) {
-      creatorValue = [creatorValue];
-      appMetadata.creator = creatorValue;
-    }
+  if (appMetadata.creator && !Array.isArray(appMetadata.creator)) {
+    appMetadata.creator = [appMetadata.creator];
   }
+
   // Add .png to screenshot path if it doesn't end with screenshot
   // Add app.screenshots[i].fullPath
   if (appMetadata.screenshots) {
