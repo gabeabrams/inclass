d3e83c5020148c1f1aa1ed1a6359ff89bea21758
stesta03 <samantha.testa@tufts.edu>
2019-06-07 11:25:29 -0400

    made colors list global and fixed logic error

--- server/Store/helpers/postProcessTags.js
+++ server/Store/helpers/postProcessTags.js
@@ -1,3 +1,16 @@
+const COLORS = [
+  '#701938',
+  '#12327c',
+  '#236842',
+  '#3c1b43',
+  '#4f3814',
+  '#ff2f92',
+  '#ff9300',
+  '#4f2563',
+  '#005392',
+  '#929000',
+];
+
 /**
  * Post-processes the tags within a catalog and its apps
  * @module server/Store/helpers/postProcessTags
@@ -7,34 +20,28 @@
  *   there was no tagName or the tagItems array was empty, we add
  *   'other/uncategorized' as the tagItem)
  */
-module.exports = async (catalog) => {
+module.exports = (catalog) => {
   // We want to get the apps and tagsToShow out of the catalog
   const { apps, tagsToShow } = catalog;
 
+  // We need to make sure tagsToShow object exists and build it if it doesn't
+  // If it doesn't exist, we need to build object from list of tags in apps
+
+  // Set object for list of tags
+
   // Make array of random colors
-  const colors = [
-    '#701938',
-    '#12327c',
-    '#236842',
-    '#3c1b43',
-    '#4f3814',
-    '#ff2f92',
-    '#ff9300',
-    '#4f2563',
-    '#005392',
-    '#929000',
-  ];
   let nextColorIndex = 0;
 
   // Now that we have apps and tagsToShow, make sure tag data is correct
   // If tag doesn't have a tagColor, give it one
   tagsToShow.forEach((tag) => {
-    if (!tag.tagColor) {
-      tag.tagColor = colors[nextColorIndex] % colors.length;
+    // If a tag doesn't have a tagColor attribute or it's empty, we will
+    // add a color from the list
+    if (!tagsToShow[tag].tagColor) {
+      tagsToShow[tag].tagColor = COLORS[nextColorIndex %  COLORS.length];
+      nextColorIndex += 1;
     }
-
-    
   });
-
-
+  const updatedCatalog = catalog;
+  updatedCatalog.tagsToShow = tagsToShow;
 };
