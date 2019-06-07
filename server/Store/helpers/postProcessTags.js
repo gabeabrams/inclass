76295c5b70d508381fb6c20c6b6f3f631417b96a
stesta03 <samantha.testa@tufts.edu>
2019-06-07 11:05:48 -0400

    picked/added app colors

--- server/Store/helpers/postProcessTags.js
+++ server/Store/helpers/postProcessTags.js
@@ -9,17 +9,31 @@
  */
 module.exports = async (catalog) => {
   // We want to get the apps and tagsToShow out of the catalog
-  const {apps, tagsToShow} = catalog;
+  const { apps, tagsToShow } = catalog;
 
   // Make array of random colors
-  const someRandomColor = [];
+  const colors = [
+    '#701938',
+    '#12327c',
+    '#236842',
+    '#3c1b43',
+    '#4f3814',
+    '#ff2f92',
+    '#ff9300',
+    '#4f2563',
+    '#005392',
+    '#929000',
+  ];
+  let nextColorIndex = 0;
 
   // Now that we have apps and tagsToShow, make sure tag data is correct
   // If tag doesn't have a tagColor, give it one
   tagsToShow.forEach((tag) => {
     if (!tag.tagColor) {
-      tag.tagColor = someRandomColor;
+      tag.tagColor = colors[nextColorIndex] % colors.length;
     }
+
+    
   });
 
 
