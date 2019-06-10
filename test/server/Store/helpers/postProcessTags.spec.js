0777e6e652ceada7f6ef08c5c0fe5265eb2cc248
stesta03 <samantha.testa@tufts.edu>
2019-06-10 14:14:46 -0400

    Added test file

---
+++ test/server/Store/helpers/postProcessTags.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+const postProcessTags = require('../../../../server/Store/helpers/postProcessTags');
+
+describe('server > Store > helpers > postProcessTags', function () {
+  
+});
