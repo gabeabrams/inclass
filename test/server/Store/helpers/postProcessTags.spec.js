35c43287a1e3ce0748d6ff5be95f37b3e75f2e04
stesta03 <samantha.testa@tufts.edu>
2019-06-10 19:20:09 -0400

    added test catalog data

--- test/server/Store/helpers/postProcessTags.spec.js
+++ test/server/Store/helpers/postProcessTags.spec.js
@@ -3,5 +3,51 @@ const assert = require('assert');
 const postProcessTags = require('../../../../server/Store/helpers/postProcessTags');
 
 describe('server > Store > helpers > postProcessTags', function () {
-  
+  // Fake Catalog
+  const testCatalog = {
+    title: 'SEAS Catalog',
+    accounts: [26, 30],
+    tagsToShow: [
+      {
+        tagName: 'cost',
+        color: 'blue',
+      },
+      {
+        tagName: 'type',
+        color: 'red',
+      },
+    ],
+    defaultSupportEmail: 'example@harvard.edu',
+    apps: [
+      {
+        name: 'AppOne',
+        tags: {
+          cost: ['free'],
+          type: ['import', 'export'],
+        },
+      },
+      {
+        name: 'AppTwo',
+        tags: {
+          cost: ['expensive'],
+          type: ['import'],
+        },
+      },
+      {
+        name: 'AppThree',
+        tags: {
+          cost: ['free'],
+          type: ['export'],
+        },
+      },
+      {
+        name: 'AppFour',
+        tags: {
+          cost: ['expensive'],
+          type: ['teaching'],
+        },
+      },
+    ],
+  };
+
 });
