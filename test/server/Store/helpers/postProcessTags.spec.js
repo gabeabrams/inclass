a7f291fa792e102c346f9aa673a7cdaf7fc99c83
stesta03 <samantha.testa@tufts.edu>
2019-06-11 16:55:02 -0400

    added testdata and tests

--- test/server/Store/helpers/postProcessTags.spec.js
+++ test/server/Store/helpers/postProcessTags.spec.js
@@ -4,19 +4,9 @@ const postProcessTags = require('../../../../server/Store/helpers/postProcessTag
 
 describe.only('server > Store > helpers > postProcessTags', function () {
   // Fake Catalog
-  const testCatalog = {
+  const testCatalogNoTagsToShow = {
     title: 'SEAS Catalog',
     accounts: [26, 30],
-    // tagsToShow: [
-    //   {
-    //     tagName: 'cost',
-    //     color: 'blue',
-    //   },
-    //   {
-    //     tagName: 'type',
-    //     color: 'red',
-    //   },
-    // ],
     defaultSupportEmail: 'example@harvard.edu',
     apps: [
       {
@@ -50,12 +40,50 @@ describe.only('server > Store > helpers > postProcessTags', function () {
     ],
   };
 
+  const testCatalogAppsMissingTags = {
+    title: 'SEAS Catalog',
+    accounts: [26, 30],
+    defaultSupportEmail: 'example@harvard.edu',
+    apps: [
+      {
+        name: 'AppOne',
+        tags: {
+          cost: ['free'],
+          type: ['import', 'export'],
+        },
+      },
+      {
+        name: 'AppTwo',
+        tags: {
+          type: ['import'],
+        },
+      },
+      {
+        name: 'AppThree',
+        tags: {
+          cost: ['free'],
+          type: ['export'],
+        },
+      },
+      {
+        name: 'AppFour',
+        tags: {
+          cost: ['expensive'],
+          type: ['teaching'],
+        },
+      },
+    ],
+  };
+
   it.only('Will update an app so it has each tag found in the tagsToShow', async function () {
-    console.log(postProcessTags(testCatalog));
+    console.log('Catalog before postProcessTags: ', JSON.stringify(testCatalogAppsMissingTags, undefined, 2));
+    console.log('Catalog after postProcessTags: ', JSON.stringify(postProcessTags(testCatalogAppsMissingTags), undefined, 2));
+
   });
 
   it('Will create a tagsToShow object if there is not already one', async function () {
-
+    console.log('Catalog before postProcessTags: ', JSON.stringify(testCatalogNoTagsToShow, undefined, 2));
+    console.log('Catalog after postProcessTags: ', JSON.stringify(postProcessTags(testCatalogNoTagsToShow), undefined, 2));
   });
 
   it('Will throw an error if a tag does not have a tagName key', async function () {
