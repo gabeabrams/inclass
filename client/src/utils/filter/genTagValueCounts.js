1438a37a23c5a112c67327592cbae3348c486fbb
Gabe Abrams <gabeabrams@gmail.com>
2019-06-14 11:30:49 -0400

    Small eslint errors, fixing audit errors

--- client/src/utils/filter/genTagValueCounts.js
+++ client/src/utils/filter/genTagValueCounts.js
@@ -6,7 +6,7 @@ const excludeTagName = require('./excludeTagName');
  * @param {object[]} apps - the list of apps after they are already filtered by
  *   the query
  * @param {object} tags - tag mapping that stores whether values are checked:
- *   { tagName => { color, tagValues: { tagValue: isChecked }}}
+ *   { tagName => { color, values: { tagValue: isChecked }}}
  * @return {object} mapping { tagName => tagItem => count } where count is
  *   the number of apps that have that tagItem while all the other tagNames are
  *   applied as filters
