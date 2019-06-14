73dc9f15490546b3b8d4066705130687d15f1692
Anita Lam <anitalam@Anitas-MacBook-Pro.local>
2019-06-14 14:33:56 -0400

    Turned testing data into a proxyquire

--- test/server/Store/index.spec.js
+++ test/server/Store/index.spec.js
@@ -1,16 +1,24 @@
 const assert = require('assert');
+const proxyquire = require('proxyquire');
+const path = require('path');
 const ExpressApp = require('../../dummy-objects/ExpressApp');
-const Store = require('../../../server/Store/index');
 const API = require('../../dummy-objects/API');
 
 const expressApp = new ExpressApp();
 const badExpressApp = 'Not Real';
+const dummyPath = path.join(__dirname, '../../dummy-data/store/medium');
+const Store = proxyquire('../../../server/Store/index', {
+  './STORE_CONSTANTS': {
+    path: dummyPath,
+    '@global': true,
+  },
+});
 const store = new Store(expressApp);
 
 describe.only('server > Store > index', function () {
   it('Checks metadata objects untouched when error occurs', async function () {
-    const successful = await store._attemptLoad();
     const badStore = new Store(badExpressApp);
+    const successful = await badStore._attemptLoad();
     if (successful.success === true
     && Object.keys(badStore.storeMetadata).length !== 0
     && Object.keys(badStore.accountIdToCatalogId).length !== 0
@@ -37,6 +45,7 @@ describe.only('server > Store > index', function () {
   });
 
   it('Checks getCatalogAndPermissions returns expected item', async function () {
+    await store._attemptLoad();
     const api = new API();
     const launchInfo = { courseId: 102 };
     const dataPermissions = await store
@@ -46,6 +55,7 @@ describe.only('server > Store > index', function () {
   });
 
   it('Checks getInstallData returns expected item', async function () {
+    await store._attemptLoad();
     const myInstallData = store.getInstallData('dce', 'gradeup');
     const expectedData = {
       name: 'Harvard Appstore',
@@ -59,11 +69,13 @@ describe.only('server > Store > index', function () {
   });
 
   it('Checks getInstallData returns null when there is no installdata for a catalog or app', async function () {
+    await store._attemptLoad();
     const myInstallData = store.getInstallData('notReal', 'fakeApp');
     assert(!myInstallData, 'getInstallData did not return null');
   });
 
   it('Checks getStoreMetadata returns expected item', async function () {
+    await store._attemptLoad();
     const myStoreMetadata = store.getStoreMetadata();
     const values = Object.values(myStoreMetadata);
     assert.equal(values[0], 'Harvard Appstore', 'getStoreMetadata did not return expected item');
