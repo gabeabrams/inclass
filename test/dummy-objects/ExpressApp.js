2a3ea698e680e9588b406b2da6a046973cda97aa
Gabe Abrams <gabeabrams@gmail.com>
2019-06-14 12:08:40 -0400

    Added functionality to fake express app and added getStore

--- test/dummy-objects/ExpressApp.js
+++ test/dummy-objects/ExpressApp.js
@@ -1,6 +1,7 @@
 class ExpressApp {
   constructor() {
     this.used = [];
+    this.getHandlers = {}; // path => handler function
   }
 
   use(path, handler) {
@@ -17,6 +18,26 @@ class ExpressApp {
       path,
       options,
     };
+  }
+
+  /**
+   * Simulate start listening to a path
+   * @param {string} path - the path to listen to
+   * @param {function} handler - the function to call when the path is hit
+   */
+  get(path, handler) {
+    this.getHandlers[path] = handler;
+  }
+
+  /**
+   * Simulates a GET response to a specific path using the given req and res
+   *   objects
+   * @param {string} path - the path to listen to
+   * @param {object} req - the request object to pass to the handler
+   * @param {object} res - the response object to pass to the handler
+   */
+  async simulateGETRequest(path, req, res) {
+    await this.getHandlers[path](req, res);
   }
 }
 module.exports = ExpressApp;
---
+++ test/dummy-objects/genStore.js
@@ -0,0 +1,38 @@
+/**
+ * Creates a fake Store class
+ * @param {object} storeMetadata - the store metadata to return
+ * @param {Catalog} catalog - the catalog to return
+ * @param {boolean} isAdmin - the isAdmin value to return
+ * @return {Store} the fake store
+ */
+module.exports = (opts) => {
+  const {
+    storeMetadata,
+    catalog,
+    isAdmin,
+  } = opts;
+
+  /* eslint-disable class-methods-use-this */
+  class Store {
+    getStoreMetadata() {
+      return storeMetadata;
+    }
+
+    async detectCatalogAndPermissions(api, launchInfo) {
+      if (!api) {
+        throw new Error('API not included');
+      }
+
+      if (!launchInfo || !launchInfo.courseId) {
+        throw new Error('Launch info not included or not defined properly');
+      }
+
+      return {
+        isAdmin,
+        catalog,
+      };
+    }
+  }
+
+  return Store;
+};
