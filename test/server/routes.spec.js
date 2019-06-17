8d156af68561fd47dbecf1d8dd753c876478f083
stesta03 <samantha.testa@tufts.edu>
2019-06-17 15:26:41 -0400

    Uncommented Gabe's test data

--- test/server/routes.spec.js
+++ test/server/routes.spec.js
@@ -15,46 +15,47 @@ const initRoutesWithStore = (expressApp, storeOpts) => {
 };
 
 describe('server > routes', function () {
-  // describe.skip('Gabe\'s example', function () {
-  //   it('Does it', async function () {
-  //     const fakeExpressApp = new ExpressApp();
-  //     const fakeAPI = new API();
-  //
-  //     const routes = initRoutesWithStore(
-  //       fakeExpressApp,
-  //       {
-  //         storeMetadata: {
-  //           title: 'My Store',
-  //         },
-  //       }
-  //     );
-  //
-  //
-  //     let jsonCalled;
-  //     let payload;
-  //     const res = {
-  //       json: (data) => {
-  //         payload = data;
-  //         jsonCalled = true;
-  //       }
-  //     };
-  //
-  //     const req = {
-  //       api: fakeAPI,
-  //       launchInfo: {
-  //         courseId: <put something here>,
-  //       },
-  //     };
-  //
-  //     fakeExpressApp.simulateGETRequest('/store', req, res);
-  //
-  //     // Call routes
-  //     routes(fakeExpressApp);
-  //   });
-  // });
+  describe.skip('Gabe\'s example', function () {
+    it('Does it', async function () {
+      const fakeExpressApp = new ExpressApp();
+      const fakeAPI = new API();
+
+      const routes = initRoutesWithStore(
+        fakeExpressApp,
+        {
+          storeMetadata: {
+            title: 'My Store',
+          },
+        }
+      );
+
+
+      let jsonCalled;
+      let payload;
+      const res = {
+        json: (data) => {
+          payload = data;
+          jsonCalled = true;
+        }
+      };
+
+      const req = {
+        api: fakeAPI,
+        launchInfo: {
+          courseId: <put something here>,
+        },
+      };
+
+      fakeExpressApp.simulateGETRequest('/store', req, res);
+
+      // Call routes
+      routes(fakeExpressApp);
+    });
+  });
 
   describe('server > routes /store', function () {
     // Sam's tests
+
   });
 
   describe('server > routes /catalog', function () {
