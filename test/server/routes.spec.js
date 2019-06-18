c06c6eac468322f951b53fff6d058a729afb5bbc
Shihan Li <lshhenry98@gmail.com>
2019-06-17 22:19:35 -0400

    added tests for cases with throwing errors

--- test/server/routes.spec.js
+++ test/server/routes.spec.js
@@ -1,4 +1,5 @@
 const proxyquire = require('proxyquire');
+const assert = require('assert');
 
 const ExpressApp = require('../dummy-objects/ExpressApp');
 const API = require('../dummy-objects/API');
@@ -18,8 +19,75 @@ const initRoutesWithStore = (expressApp, storeOpts) => {
 };
 
 describe('server > routes', function () {
-  describe('server > routes /catalog', function () {
-    it.only('tests catalog route', async function () {
+  describe.only('server > routes /catalog', function () {
+    it('throws an error if API is missing', async function () {
+      const fakeExpressApp = new ExpressApp();
+      const fakeAPI = new API();
+      initRoutesWithStore(
+        fakeExpressApp,
+        {
+          storeMetadata: {
+            title: 'Harvard Store',
+          },
+        }
+      );
+      const req = {
+        session: {
+          launchInfo: {
+            courseId: 60,
+          },
+        },
+      };
+      let dataReturnedToClient;
+      const res = {
+        json: (data) => {
+          dataReturnedToClient = data;
+        },
+      };
+      let error;
+      try {
+        await fakeExpressApp.simulateGETRequest('/catalog', req, res);
+      } catch (err) {
+        error = err;
+      }
+      assert(error, 'catalog route did not throw an error with incomplete launch information');
+    });
+
+    it('throws an error if launchInfo is in wrong format', async function () {
+      const fakeExpressApp = new ExpressApp();
+      const fakeAPI = new API();
+      // init store with only metadata
+      initRoutesWithStore(
+        fakeExpressApp,
+        {
+          storeMetadata: {
+            title: 'Harvard Store',
+          },
+        }
+      );
+
+      const req = {
+        api: fakeAPI,
+        session: {
+          courseId: 60,
+        },
+      };
+      let dataReturnedToClient;
+      const res = {
+        json: (data) => {
+          dataReturnedToClient = data;
+        },
+      };
+      let error;
+      try {
+        await fakeExpressApp.simulateGETRequest('/catalog', req, res);
+      } catch (err) {
+        error = err;
+      }
+      assert(error, 'catalog route did not throw an error with incorrect launchInformation format');
+    });
+
+    it('returns success when ', async function () {
       const fakeExpressApp = new ExpressApp();
       const fakeAPI = new API();
       // init store with only metadata
