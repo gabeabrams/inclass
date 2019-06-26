1762f4e5d74af8cb6cafb8c11cbe7a2dc34fa8e4
Gabe Abrams <gabeabrams@gmail.com>
2019-06-26 17:42:52 -0400

    Added skeleton code for store integration test

---
+++ test/selenium/integration/server/storeEndpoint.js
@@ -0,0 +1,15 @@
+require('dce-selenium');
+
+const { courseId } = require('../../../../config/devEnvironment');
+
+describeS('Server', function () {
+  itS('Responds with Store Metadata', async function (driver) {
+    await driver.visit(`https://localhost:8088/courses/${courseId}`);
+    // Click "Simulate Launch"
+    // Click "Authorize"
+    // Wait 2s
+    // Visit https://localhost/store
+    // Get the json
+    // Test the json
+  });
+});
