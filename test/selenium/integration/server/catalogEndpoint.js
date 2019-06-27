4c97642e956fe774e272f025ba63e579b64ecd65
Shihan Li <lshhenry98@gmail.com>
2019-06-27 12:20:53 -0400

    added getting JSON from catalog route

--- test/selenium/integration/server/catalogEndpoint.js
+++ test/selenium/integration/server/catalogEndpoint.js
@@ -4,12 +4,18 @@ const { courseId } = require('../../../../config/devEnvironment');
 
 describeS('Server', function () {
   itS('Responds with Catalog', async function (driver) {
+    // https://localhost/catalog
     await driver.visit(`https://localhost:8088/courses/${courseId}`);
     // Click "Simulate Launch"
+    await driver.click('#launch-button');
     // Click "Authorize"
+    await driver.click('#authorize-button');
     // Wait 2s
+    await driver.wait(2000);
     // Visit https://localhost/catalog
+    await driver.visit('https://localhost/catalog');
     // Get json from page
+    const catalog = await driver.getJSON();
     // Test the json to see if it matches data in
     //   /test/dummy-data/store/installable
   });
