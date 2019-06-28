d1c421db3edafb78cf685b8d9af96058d27e310f
stesta03 <samantha.testa@tufts.edu>
2019-06-28 11:52:10 -0400

    clicked simulate launch button

--- test/selenium/integration/server/storeEndpoint.js
+++ test/selenium/integration/server/storeEndpoint.js
@@ -6,7 +6,9 @@ describeS('Server', function () {
   itS('Responds with Store Metadata', async function (driver) {
     await driver.visit(`https://localhost:8088/courses/${courseId}`);
     // Click "Simulate Launch"
+    await driver.click('#launch-button');
     // Click "Authorize"
+
     // Wait 2s
     // Visit https://localhost/store
     // Get the json
