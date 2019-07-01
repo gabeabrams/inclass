b12e556bfc6ac395552f377aafed6e838020b1ac
Anita Lam <anita.lam@tufts.edu>
2019-07-01 00:30:14 -0400

    deleted the apps

--- test/selenium/integration/server/appLifecycle.js
+++ test/selenium/integration/server/appLifecycle.js
@@ -25,7 +25,7 @@ describeS('Server', function () {
     // wait for data in firefox to load
     await driver.wait(1000);
     // Installs an app (Note: safari can't do this?)
-    await driver.post('https://localhost/install/notinstalled');
+    await driver.post(`https://localhost/install/notinstalled`);
     // Wait for JSON data in firefox from installing an app
     await driver.wait(1000);
     // Gets the JSON and makes sure success is true
@@ -35,16 +35,20 @@ describeS('Server', function () {
     await driver.visit('https://localhost/installed-apps');
     // Wait for JSON data in firefox from installing an app
     await driver.wait(1000);
-    await driver.pause();
     // get the json and make sure success is true
     const jsonInstalledApps = await driver.getJSON();
     assert(jsonInstalledApps.success, 'success is not true for JSON data');
     const { apps } = jsonInstalledApps;
-    
-    // // remember the ltiIds for the app you just installed
-    // const ltiIds = ;
+    // remember the ltiIds for the app you just installed
+    let toUninstall;
+    apps.forEach((app) => {
+      if (app.appId === 'notinstalled') {
+        toUninstall = app.ltiIds;
+      }
+    });
     // Uninstall the app by using: driver.delete with 'https://localhost/uninstall'
     //   and include a body with ltiIds: [30578, 30894]
+    await driver.delete('https://localhost/uninstall', toUninstall);
     // Check that the app was uninstalled: driver.visit the '/installed-apps'
     //   page and make sure the app is gone
 
