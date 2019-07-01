821024ab02e995b00b66320d3060de281470be9f
Gabe Abrams <gabeabrams@gmail.com>
2019-07-01 12:14:22 -0400

    Removed extra waits for Firefox json

--- test/selenium/integration/server/appLifecycle.js
+++ test/selenium/integration/server/appLifecycle.js
@@ -4,20 +4,14 @@ const assert = require('assert');
 describeS('Server', function () {
   itS('Successfully Completes App Lifecycle', async function (driver) {
     await driver.launchAppStore();
-    // wait for data in firefox to load
-    await driver.wait(1000);
 
     // Installs an app
     await driver.post('https://localhost/install/gradeup');
-    // Wait for JSON data in firefox from installing an app
-    await driver.wait(1000);
     // Gets the JSON and makes sure success is true
     const json = await driver.getJSON();
     assert(json.success, 'success is not true for JSON data');
     // // Check if the app is installed: driver.visit the '/installed-apps' page
     await driver.visit('https://localhost/installed-apps');
-    // Wait for JSON data in firefox from installing an app
-    await driver.wait(1000);
     // get the json and make sure success is true
     const jsonInstalledApps = await driver.getJSON();
     assert(jsonInstalledApps.success, 'success is not true for JSON data');
@@ -38,7 +32,6 @@ describeS('Server', function () {
     // Check that the app was uninstalled: driver.visit the '/installed-apps'
     //   page and make sure the app is gone
     await driver.visit('https://localhost/installed-apps');
-    await driver.wait(1000);
     const jsonNewInstalledApps = await driver.getJSON();
     assert(jsonNewInstalledApps.success, 'success is not true for JSON data');
     const newApps = jsonNewInstalledApps.apps;
