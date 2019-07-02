dd51ceeb65ef5dd55488884a20461c74b91fce4f
Gabe Abrams <gabeabrams@gmail.com>
2019-07-02 15:04:53 -0400

    Handles case where Firefox crashes because front end is not running

--- test/selenium/commands.js
+++ test/selenium/commands.js
@@ -5,12 +5,19 @@ module.exports = {
     // Visit the launch simulator page
     await this.visit(`https://localhost:8088/courses/${courseId}`);
     // Clicks "Simulate Launch"
-    await this.click('.launch-button');
-    // Wait for authorization page to (maybe) load
-    await this.wait(1000);
-    // Clicks "Authorize"
-    if (await this.elementExists('.authorize-button')) {
-      await this.click('.authorize-button');
+    try {
+      await this.click('.launch-button');
+      // Wait for authorization page to (maybe) load
+      await this.wait(1000);
+      // Clicks "Authorize"
+      if (await this.elementExists('.authorize-button')) {
+        await this.click('.authorize-button');
+      }
+    } catch (err) {
+      const clientNotRunning = err.message.includes('Reached error page: about:neterror?e=connectionFailure');
+      if (!clientNotRunning) {
+        throw err;
+      }
     }
     // Waits 2s for server to finish loading
     await this.wait(2000);
