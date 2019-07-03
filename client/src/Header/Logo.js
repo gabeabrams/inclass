75005c48ea07283077395304e2b51154849bd785
stesta03 <samantha.testa@tufts.edu>
2019-07-03 10:47:16 -0400

    deconstructed props

--- client/src/Header/Logo.js
+++ client/src/Header/Logo.js
@@ -1,8 +1,15 @@
+// Import React and Component from react package
 import React, { Component } from 'react';
+
+// Import PropTypes package
 import PropTypes from 'prop-types';
 
 class Logo extends Component {
   render() {
+    // Deconstruct props
+    const {
+      url,
+    } = this.props;
     return (
       <span
         style={{
