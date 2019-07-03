d0a91f2774d9634c30a97c1cb927428de93dadeb
Anita Lam <anita.lam@tufts.edu>
2019-07-03 17:04:26 -0400

    added screenshot module

--- client/src/Body/AppPage/AppPageContent/TabBox/Screenshots/Screenshot.js
+++ client/src/Body/AppPage/AppPageContent/TabBox/Screenshots/Screenshot.js
@@ -1,2 +1,27 @@
 // props:
 // - screenshot: { title, url }
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Screenshot extends Component {
+  render() {
+    // Deconstruct props
+    const { title, url } = this.props;
+    return (
+      <div className="card">
+        <img className="card-img-top" src={url} alt="Screenshot" />
+        <div className="card-body">
+          <p className="card-text">{title}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+Screenshot.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
+export default Screenshot;
