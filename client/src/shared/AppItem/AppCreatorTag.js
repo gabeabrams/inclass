75d6ea1a0dba423b0873b67ccb072df3eb000709
Shihan Li <lshhenry98@gmail.com>
2019-07-03 21:17:47 -0400

    added first try of appCreatorTag

--- client/src/shared/AppItem/AppCreatorTag.js
+++ client/src/shared/AppItem/AppCreatorTag.js
@@ -4,15 +4,37 @@ import PropTypes from 'prop-types';
 class AppCreatorTag extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      message: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { creator } = this.props;
+    if (creator.length === 1) {
+      this.setState({
+        message: `By ${creator[0]}`,
+      });
+    } else {
+      let newMessage = `By ${creator[0]}`;
+      for (let i = 1; i < creator.length; i++) {
+        newMessage = newMessage.concat(
+          (i === creator.length - 1)
+            ? `, and ${creator[i]}`
+            : `, ${creator[i]}`
+        );
+      }
+      this.setState({
+        message: newMessage,
+      });
+    }
   }
 
   render() {
-    // Message: 26 apps match your search
-    const { creator } = this.props;
-
+    const { message } = this.state;
     return (
       <div className="alert alert-warning">
-        No status bar
+        {message}
       </div>
     );
   }
