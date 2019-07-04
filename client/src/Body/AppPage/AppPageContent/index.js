a5579d60ebae5b23ad92d5d8ef0a4e99aad4cff1
Anita Lam <anita.lam@tufts.edu>
2019-07-03 20:22:27 -0400

    Added import flow from Screenshot to AppPage

--- client/src/Body/AppPage/AppPageContent/index.js
+++ client/src/Body/AppPage/AppPageContent/index.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // Import other components
-import Screenshots from './Screenshots';
-class TabBox extends Component {
+import TabBox from './TabBox';
+
+class AppPageContent extends Component {
   render() {
     return ();
   }
 }
 
-export default TabBox;
+export default AppPageContent;
--- client/src/Body/AppPage/index.js
+++ client/src/Body/AppPage/index.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 // Import other components
 import AppPageFooter from './AppPageFooter';
+import AppPageContent from './AppPageContent';
 
 class AppPage extends Component {
   render() {
     return (
       <div>
+        <AppPageContent />
         <AppPageFooter />
       </div>
     );
