b84ae3ce58fab2530c6263a9bb66e9beffddca68
Anita Lam <anita.lam@tufts.edu>
2019-07-03 20:17:48 -0400

    imported screenshot to screenshots, began implementing screenshots, set up TabBox

--- client/src/Body/AppPage/AppPageContent/TabBox/Screenshots/index.js
+++ client/src/Body/AppPage/AppPageContent/TabBox/Screenshots/index.js
@@ -1,2 +1,34 @@
 // props:
 // - app (the app to show the screenshots of)
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import other components
+// Questions to answer: How to get the app to show the screenshots of?
+import Screenshot from './Screenshot';
+
+
+class Screenshots extends Component {
+  render() {
+    getScreenshots = () => {
+      const { screenshots } = this.props.app;
+      screenshots.forEach( (screenshot) => {
+        const { title, filename } = screenshot;
+              <div> 
+                <Screenshot src={filename} {title} />
+              </div>
+      });
+    }
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+Screenshots.propTypes = {
+  app: PropTypes.node.isRequired,
+};
+
+export default Screenshots;
---
+++ client/src/Body/AppPage/AppPageContent/TabBox/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import other components
+class TabBox extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default TabBox;
