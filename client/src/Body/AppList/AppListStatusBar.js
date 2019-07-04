541f6c672be0eb1479ab4011ef59d0ef2bfb17aa
Shihan Li <lshhenry98@gmail.com>
2019-07-03 20:36:42 -0400

    added status bar component

--- client/src/Body/AppList/AppListStatusBar.js
+++ client/src/Body/AppList/AppListStatusBar.js
@@ -4,27 +4,29 @@ import PropTypes from 'prop-types';
 class AppListStatusBar extends Component {
   constructor(props) {
     super(props);
-    // ...
+    this.state = {
+      message: (
+        props.appCount === 0
+          ? 'There are no matching apps. Please broaden your search criteria'
+          : `${props.appCount} apps match your search`
+      ),
+    };
   }
 
   render() {
-    // Message: 26 apps match your search
-    const { appCount } = this.props;
-
+    // deconstruct state
+    const { message } = this.state;
     return (
       <div className="alert alert-warning">
-        No status bar
+        {message}
       </div>
     );
   }
 }
-
 AppListStatusBar.propTypes = {
   appCount: PropTypes.number,
 };
-
 AppListStatusBar.defaultProps = {
   appCount: 0,
 };
-
 export default AppListStatusBar;
