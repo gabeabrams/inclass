2f7a31bf21f154e2db115b91bf74648e45e1b166
Shihan Li <lshhenry98@gmail.com>
2019-07-07 12:53:21 -0400

    changed props naming so not using reserved key word

--- client/src/shared/AppItem/AppTags/AppTag.js
+++ client/src/shared/AppItem/AppTags/AppTag.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 class AppTag extends Component {
   render() {
     // deconstruct props
-    const { key, value } = this.props;
+    const { tagKey, tagValue } = this.props;
     return (
       <div className="alert alert-warning">
-        {'key is ' + key + 'value is ' + value}
+        <h1>{'key is ' + tagKey + ' value is ' + tagValue}</h1>
       </div>
     );
   }
 }
 AppTag.propTypes = {
-  key: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  tagKey: PropTypes.string.isRequired,
+  tagValue: PropTypes.string.isRequired,
 };
 export default AppTag;
