53acb32db3df3257241baacb8f31db6d562885f4
Shihan Li <lshhenry98@gmail.com>
2019-07-08 15:09:12 -0400

    added css to appTag

--- client/src/shared/AppItem/AppTags/AppTag.js
+++ client/src/shared/AppItem/AppTags/AppTag.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './AppTag.css';
 
 class AppTag extends Component {
   render() {
     // deconstruct props
     const { tagKey, tagValue } = this.props;
     return (
-      <div className="appTag">
-        {'key is ' + tagKey + ' value is ' + tagValue}
+      <div className="alert alert-secondary">
+        <span className="key-label">{tagKey}</span>
+        <span className="value-label">{tagValue}</span>
       </div>
     );
   }
