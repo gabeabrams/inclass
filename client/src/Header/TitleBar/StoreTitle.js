841f02253a0ffc06509bbb03e15f5d0e328e3fb5
stesta03 <samantha.testa@tufts.edu>
2019-07-09 19:25:29 -0400

    deconstruct storeTitle from props

--- client/src/Header/TitleBar/StoreTitle.js
+++ client/src/Header/TitleBar/StoreTitle.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types';
 
 class StoreTitle extends Component {
   render() {
+    // Deconstruct props
+    const {
+      storeTitle,
+    } = this.props;
+
     return (
+      <div>
+        { storeTitle }
+      </div>
 
     );
   }
-
 }
+
+StoreTitle.PropTypes = {
+  storeTitle: PropTypes.string.isRequired,
+};
+
+export default StoreTitle;
