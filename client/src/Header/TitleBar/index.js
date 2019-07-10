d193c6ca0a1fff8001c9720d772fc1f95ed0069b
stesta03 <samantha.testa@tufts.edu>
2019-07-09 21:36:26 -0400

    began skeleton for titlebar component

---
+++ client/src/Header/TitleBar/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import other components
+import StoreTitle from './StoreTitle';
+import CatalogTitle from './CatalogTitle';
+
+class TitleBar extends Component {
+  render() {
+    // Deconstruct any props needed by titlebar or its children
+    const {
+
+    } = this.props;
+
+    return (
+
+    );
+  }
+}
