361c66fa9187a4c968de63ba338c6bd0da90bfbd
stesta03 <samantha.testa@tufts.edu>
2019-07-09 23:43:50 -0400

    imported stylesheet, added props storeTitle and catalogTitle

--- client/src/Header/TitleBar/index.js
+++ client/src/Header/TitleBar/index.js
@@ -5,15 +5,30 @@ import PropTypes from 'prop-types';
 import StoreTitle from './StoreTitle';
 import CatalogTitle from './CatalogTitle';
 
+// Import stylesheet
+import './style.css';
+
 class TitleBar extends Component {
   render() {
     // Deconstruct any props needed by titlebar or its children
     const {
-
+      storeTitle,
+      catalogTitle,
     } = this.props;
 
     return (
+      <div
+        className=titlebar
+      >
+        <StoreTitle storeTitle=storeTitle>
+        <CatalogTitle catalogTitle=catalogTitle>
+      </div>
 
     );
   }
+}
+
+TitleBar.propTypes = {
+  storeTitle: PropTypes.string.isRequired,
+  catalogTitle: PropTypes.string.isRequired,
 }
