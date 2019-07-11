d081c42d656692e1e287b77ab17cf604e9506749
Anita Lam <anita.lam@tufts.edu>
2019-07-11 11:43:10 -0700

    Created placeholders for Guides and Info Page

---
+++ client/src/Body/AppPage/AppPageContent/TabBox/Info.js
@@ -0,0 +1,22 @@
+// props:
+// - app (the app to show the screenshots of)
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import other components
+// Questions to answer: How to get the app to show the screenshots of?
+
+class Info extends Component {
+  render() {
+    return (
+      'This is a placeholder for the Info page'
+    );
+  }
+}
+
+Info.propTypes = {
+
+};
+
+export default Info;
