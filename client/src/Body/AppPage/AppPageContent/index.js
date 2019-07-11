21259fde20d4fc2dd8d6ef7a702e89b24d21e411
Gabe Abrams <gabeabrams@gmail.com>
2019-07-11 08:55:21 -0700

    Added todos for tabbed app page structure

--- client/src/Body/AppPage/AppPageContent/index.js
+++ client/src/Body/AppPage/AppPageContent/index.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // Import other components
-import TabBox from './TabBox';
+import Screenshots from './TabBox/Screenshots';
+
+// Names of tabs
+import TAB_NAMES from './TAB_NAMES';
 
 class AppPageContent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTab: TAB_NAMES.SCREENSHOTS,
+    };
+
+    this.tabChanged = this.tabChanged.bind(this);
+  }
+
+  tabChanged(newTabName) {
+    this.setState({
+      currentTab: newTabName,
+    });
+  }
+
   render() {
     const { app } = this.props;
+    // TODO: show the current tab box (screenshots, guides, or info)
+    // TODO: pass this.tabChanged to the tab bar so it can call it when a new tab is selected
     return (
       <div>
         <TabBox app={app} />
