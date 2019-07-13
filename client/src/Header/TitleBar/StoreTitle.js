09847e9a8653f5281b3756613108576faee9b4c1
stesta03 <samantha.testa@tufts.edu>
2019-07-13 13:22:04 -0400

    imported css

--- client/src/Header/TitleBar/StoreTitle.js
+++ client/src/Header/TitleBar/StoreTitle.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// Import css
+import './StoreTitle.css';
+
 class StoreTitle extends Component {
   render() {
     // Deconstruct props
