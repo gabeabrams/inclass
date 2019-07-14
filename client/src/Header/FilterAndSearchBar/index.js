217204ebb73d1be873ebb526245b9126640c51e1
stesta03 <samantha.testa@tufts.edu>
2019-07-14 19:58:11 -0400

    imported other components to FilterandSearchBar

--- client/src/Header/FilterAndSearchBar/index.js
+++ client/src/Header/FilterAndSearchBar/index.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// Import other components
+import FilterToggle from './FilterToggle';
+import SearchField from './SearchField';
+
 // Import css
 import './style.css';
 
 class FilterAndSearchBar extends Component {
   render() {
     // Deconstruct what we'll need from Header
-    const {
-
-    } = this.props;
 
     return (
       <div className="filterandsearchbar-container row no-gutters">
-        <div className="col filterandsearchbar-filterbutton"></div>
-        <div className="col filterandsearchbar-searchbox"></div>
+        <div className="col filterandsearchbar-filterbutton" >
+          <FilterToggle
+        <div className="col filterandsearchbar-searchbox" />
       </div>
     );
   }
 }
+
+export default FilterAndSearchBar;
