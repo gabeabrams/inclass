712f87b049112466a9541b67dd26df2a0742d76c
Gabe Abrams <gabeabrams@gmail.com>
2019-07-17 10:28:38 -0400

    Minor changes to styling

--- client/src/Header/TitleBar/CatalogTitle.js
+++ client/src/Header/TitleBar/CatalogTitle.js
@@ -13,10 +13,13 @@ class CatalogTitle extends Component {
     } = this.props;
 
     return (
-      <div className="catalogtitle-container row no-gutters">
-        {/* Used col-{breakpoint}-auto to make column fit to content */}
-        <div className="col-{breakpoint}-auto"><b>{ catalogTitle }</b></div>
-        <div className="catalogtitle-catalogtext d-none d-sm-block col">Catalog</div>
+      <div className="catalogtitle-container">
+        <span className="font-weight-bold mr-1">
+          {catalogTitle}
+        </span>
+        <span className="d-none d-sm-inline">
+          Catalog
+        </span>
       </div>
     );
   }
