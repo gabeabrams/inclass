31b7df65b2f22e90912772af75b7ac9589f619bf
stesta03 <samantha.testa@tufts.edu>
2019-07-18 18:28:38 -0400

    included font awesome icon in filter

--- client/src/Header/FilterAndSearchBar/FilterToggle.js
+++ client/src/Header/FilterAndSearchBar/FilterToggle.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+
 
 // Import css
 import './FilterToggle.css';
@@ -10,7 +14,7 @@ class FilterToggle extends Component {
       <div className="filtertoggle-container">
         <span className="text-white font-weight-bold">Filters</span>
         <span className="text-white font-weight-bold ml-1">& Tags</span>
-        <span className="ml-1">Hi</span>
+        <FontAwesomeIcon icon={faCaretRight} size="2x" className="ml-3" />
       </div>
     );
   }
