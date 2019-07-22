7657c5d36c0f97b6797c9b3fb8d1b32302e6dd45
Anita Lam <anita.lam@tufts.edu>
2019-07-22 10:49:21 -0400

    Set up new testing file for TabBar

---
+++ client/src/Body/AppPage/AppPageContent/TabBar.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Driver from 'dce-enzyme';
+import assert from 'assert';
+
+import TabBar from './TabBar';
+
+describe('client > src > Body > AppPage > AppPageContent > TabBar', () => {
+  it('Handles a click on screenshots tab', async () => {
+
+  });
+
+  it('Handles a click on guides tab', async () => {
+
+  });
+
+  it('Handles a click on info tab', async () => {
+
+  });
+});
