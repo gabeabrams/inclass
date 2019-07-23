e895b25936fc312ebe270faae4ebd6a7095fb2b1
Shihan Li <lshhenry98@gmail.com>
2019-07-23 13:37:35 -0400

    added skeleton of app creator tag

---
+++ client/src/shared/AppItem/AppCreatorTag.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Driver from 'dce-enzyme';
+import assert from 'assert';
+
+import AppCreatorTag from './AppCreatorTag';
+
+describe('client > Logo', () => {
+  it('Displays the AppStore Logo', async () => {
+    const driver = new Driver(
+    );
+  });
+});
