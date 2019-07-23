a6aa0b66f66da6a38f558f8037d151c42a849e67
Shihan Li <lshhenry98@gmail.com>
2019-07-23 13:44:20 -0400

    added test that app creator tag renders

--- client/src/shared/AppItem/AppCreatorTag.spec.js
+++ client/src/shared/AppItem/AppCreatorTag.spec.js
@@ -4,9 +4,12 @@ import assert from 'assert';
 
 import AppCreatorTag from './AppCreatorTag';
 
-describe('client > Logo', () => {
-  it('Displays the AppStore Logo', async () => {
+describe('client > src > shared > AppCreatorTag', () => {
+  it('Displays the AppCreatorTag', async () => {
     const driver = new Driver(
+      <AppCreatorTag creator="hello" dark />
     );
+    // check if app creator tag is rendered on page
+    assert(driver.elementExists('.appcreatortag-box'), 'did not render correctly');
   });
 });
