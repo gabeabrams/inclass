9b07bec85a5b6f7bf83dc9dd60f3906260cfb281
Shihan Li <lshhenry98@gmail.com>
2019-07-23 13:47:42 -0400

    slightly changed comments

--- client/src/shared/AppItem/AppCreatorTag.spec.js
+++ client/src/shared/AppItem/AppCreatorTag.spec.js
@@ -13,11 +13,11 @@ describe('client > src > shared > AppCreatorTag', () => {
     assert(driver.elementExists('.appcreatortag-box'), 'did not render correctly');
   });
 
-  it('Displays the AppCreatorTag with light background when dark not passed in', () => {
+  it('Displays with light background when dark is not passed in', () => {
     const driver = new Driver(
       <AppCreatorTag creator="hello" />
     );
-    // check if app creator tag is rendered on page
+    // check if app creator tag is rendered with light background
     assert(driver.elementExists('.badge-light'), 'did not render correctly');
   });
 });
