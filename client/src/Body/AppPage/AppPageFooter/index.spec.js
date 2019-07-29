41465beeebbdc47aa7e3005421d83aaa7bb75efb
Anita Lam <anita.lam@tufts.edu>
2019-07-29 14:57:04 -0400

    implemented testing for AppPageFooter

--- client/src/Body/AppPage/AppPageFooter/index.spec.js
+++ client/src/Body/AppPage/AppPageFooter/index.spec.js
@@ -5,11 +5,51 @@ import assert from 'assert';
 import AppPageFooter from './index';
 
 describe('client > src > Body > AppPage > AppPageFooter', () => {
-  it('Checks buttons exist', async () => {
-
-    // initializes the driver with the install button
+  it('Checks install button exists when app is not installed', async () => {
+    let appInstalled = false;
+    const supportClicked = true;
+    // initializes the driver with the footer button
     const driver = new Driver(
-      <AppPageFooter />
+      <AppPageFooter
+        appInstalled={appInstalled}
+        onInstallClicked={() => {
+          appInstalled = true;
+        }}
+        onUninstallClicked={() => {
+          appInstalled = false;
+        }}
+        onSupportClicked={() => {
+          return supportClicked;
+        }}
+      />
     );
+
+    assert(driver.elementExists('#install-button'), 'install button does not exist before click');
+    assert(!driver.elementExists('#uninstall-button'), 'uninstall button exists when it should not');
+    assert(driver.elementExists('#support-button'), 'support button does not exist');
+  });
+
+  it('Checks uninstall button exists when app is installed', async () => {
+    let appInstalled = true;
+    const supportClicked = true;
+    // initializes the driver with the footer button
+    const driver = new Driver(
+      <AppPageFooter
+        appInstalled={appInstalled}
+        onInstallClicked={() => {
+          appInstalled = true;
+        }}
+        onUninstallClicked={() => {
+          appInstalled = false;
+        }}
+        onSupportClicked={() => {
+          return supportClicked;
+        }}
+      />
+    );
+
+    assert(!driver.elementExists('#install-button'), 'install button does not exist before click');
+    assert(driver.elementExists('#uninstall-button'), 'uninstall button exists when it should not');
+    assert(driver.elementExists('#support-button'), 'support button does not exist');
   });
 });
