1ab879e91cb429415e89904ff8e6cda4a44eaa62
Anita Lam <anita.lam@tufts.edu>
2019-07-29 14:22:53 -0400

    Added updated AppPage index.js for testing purposes

--- client/src/Body/AppPage/index.js
+++ client/src/Body/AppPage/index.js
@@ -5,18 +5,52 @@ import PropTypes from 'prop-types';
 import AppPageFooter from './AppPageFooter';
 import AppPageContent from './AppPageContent';
 
+import './style.css';
+
+// Note: this is a temporary function
+const onSupportClicked = () => {
+  console.log('clicked support button');
+};
 class AppPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      appInstalled: false,
+    };
+
+    this.onInstallClicked = this.onInstallClicked.bind(this);
+    this.onUninstallClicked = this.onUninstallClicked.bind(this);
+  }
+
+  onInstallClicked() {
+    this.setState({
+      appInstalled: true,
+    });
+  }
+
+  onUninstallClicked() {
+    this.setState({
+      appInstalled: false,
+    });
+  }
+
   render() {
     // Deconstruct props
     const { app } = this.props;
+    const { appInstalled } = this.state;
     return (
       <div className="appPage-container">
         <div className="appPage-content">
           <AppPageContent app={app} />
         </div>
         <div className="appPage-footer">
-          {/* TODO: Pass in the functions for the AppPageFooter */}
-          <AppPageFooter />
+          <AppPageFooter
+            appInstalled={appInstalled}
+            onInstallClicked={this.onInstallClicked}
+            onUninstallClicked={this.onUninstallClicked}
+            onSupportClicked={onSupportClicked}
+          />
         </div>
       </div>
     );
