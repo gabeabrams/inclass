f1bf973559b52270662cf122591e8483f0538203
stesta03 <samantha.testa@tufts.edu>
2019-08-01 12:19:03 -0400

    Added test to check url of image

--- client/src/Header/Logo.spec.js
+++ client/src/Header/Logo.spec.js
@@ -1,12 +1,11 @@
 // Using enzyme
-// Write tests alongside their components.js in the folders
 import React from 'react';
 import Driver from 'dce-enzyme';
 import assert from 'assert';
 
 import Logo from './Logo';
 
-describe('client > Logo', () => {
+describe('client > src > Header > Logo', () => {
   it('Displays the AppStore Logo', async () => {
     const driver = new Driver(
       <Logo
@@ -14,7 +13,13 @@ describe('client > Logo', () => {
       />
     );
 
+    const url = 'https://www.google.com/search?q=harvard+logo&prmd=isnv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv_rCRxsfjAhUFuVkKHf3TAKYQ_AUoAXoECBIQAQ&biw=870&bih=701&dpr=2#imgrc=DYmtqTjJwjBb2M';
+    // Make sure that the logo container is rendered
     await assert(driver.elementExists('.logo-container'), 'Logo container not rendering');
+    // Make sure that logo image is rendered
     await assert(driver.elementExists('.logo-image'), 'Logo image not rendering');
+    // Make sure url is correct
+    const att = driver.getAttributes('.logo-image');
+    await assert.equal(att.src, url, 'The url is not correct');
   });
 });
