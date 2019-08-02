82300fa6fbd931b8b213fda153805e901746c4cd
Shihan Li <lshhenry98@gmail.com>
2019-08-02 10:31:26 -0400

    added modal backdrop, styled it to grey

--- client/src/Modal/index.js
+++ client/src/Modal/index.js
@@ -15,35 +15,43 @@ class Modal extends Component {
     } = this.props;
 
     return (
-      <div className="modal d-block" id="modal" tabIndex="-1" role="dialog">
-        <div className="modal-dialog modal-lg" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">{title}</h5>
-              <button
-                type="button"
-                className="close"
-                onClick={onClose}
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div
-              className="modal-body"
-              style={{
-                maxHeight: '70vh',
-                overflowY: 'scroll',
-                overflowX: 'hidden',
-              }}
-            >
-              {children}
-            </div>
-            {footer && (
-              <div className="modal-footer">
-                {footer}
+      <div>
+        <div
+          className="modal-backdrop"
+          style={{
+            opacity: 0.5,
+          }}
+        />
+        <div className="modal d-block" id="modal" tabIndex="-1" role="dialog">
+          <div className="modal-dialog modal-lg" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{title}</h5>
+                <button
+                  type="button"
+                  className="close"
+                  onClick={onClose}
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </div>
-            )}
+              <div
+                className="modal-body"
+                style={{
+                  maxHeight: '70vh',
+                  overflowY: 'scroll',
+                  overflowX: 'hidden',
+                }}
+              >
+                {children}
+              </div>
+              {footer && (
+                <div className="modal-footer">
+                  {footer}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
