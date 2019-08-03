a132a0efd73b6e77c7cccf1820804d01b6a2ad4e
Shihan Li <lshhenry98@gmail.com>
2019-08-02 22:19:18 -0400

    modified test for creator box

--- client/src/shared/AppItem/AppCreatorTag.spec.js
+++ client/src/shared/AppItem/AppCreatorTag.spec.js
@@ -7,17 +7,19 @@ import AppCreatorTag from './AppCreatorTag';
 describe('client > src > shared > AppCreatorTag', () => {
   it('Displays the AppCreatorTag', async () => {
     const driver = new Driver(
-      <AppCreatorTag creator="hello" dark />
+      <AppCreatorTag creator={['hello']} dark />
     );
     // check if app creator tag is rendered on page
     assert(driver.elementExists('.appcreatortag-box'), 'did not render correctly');
+    assert.equal(driver.getText('.appcreatortag-box'), 'by hello', 'did not display the correct message');
   });
 
   it('Displays with light background when dark is not passed in', () => {
     const driver = new Driver(
-      <AppCreatorTag creator="hello" />
+      <AppCreatorTag creator={['world']} />
     );
     // check if app creator tag is rendered with light background
     assert(driver.elementExists('.badge-light'), 'did not render correctly');
+    assert.equal(driver.getText('.appcreatortag-box'), 'by world', 'did not display the correct message');
   });
 });
