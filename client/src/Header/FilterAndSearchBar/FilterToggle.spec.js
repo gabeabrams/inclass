613edd93defa5ece4c0428d7b0c066880ebe4fdc
stesta03 <samantha.testa@tufts.edu>
2019-08-03 14:04:09 -0400

    added comments

--- client/src/Header/FilterAndSearchBar/FilterToggle.spec.js
+++ client/src/Header/FilterAndSearchBar/FilterToggle.spec.js
@@ -15,10 +15,11 @@ describe('client > src > Header > FilterAndSearchBar > FilterToggle', () => {
     );
 
     // Make sure that the caret being rendered is the sideways caret
+    // because filterDrawerOpen is false
     await assert(driver.elementExists('.fa-caret-right'), 'Caret element is missing');
   });
   it('Has down-facing caret if filterDrawerOpen is true', async () => {
-
+    // Initialize Driver
     const driver = new Driver(
       <FilterToggle
         filterDrawerOpen
@@ -26,7 +27,8 @@ describe('client > src > Header > FilterAndSearchBar > FilterToggle', () => {
       />
     );
 
-    // Make sure that the caret being rendered is the down caret
+    // Make sure that the caret being rendered is the down caret because
+    // filterDrawerOpen is true
     await assert(driver.elementExists('.fa-caret-down'), 'Caret element is missing');
   });
   it('Handles a click', async () => {
@@ -42,6 +44,7 @@ describe('client > src > Header > FilterAndSearchBar > FilterToggle', () => {
       />
     );
 
+    // Make sure that button was clicked
     await driver.click('button');
     assert(clicked, 'Button was not clicked');
   });
