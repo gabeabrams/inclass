4bcb9758464fc4efd5204c6f92a631f2d45b9afd
Shihan Li <lshhenry98@gmail.com>
2019-08-02 22:15:55 -0400

    added test for appliststatusbar

--- client/src/Body/AppList/AppListStatusBar.spec.js
+++ client/src/Body/AppList/AppListStatusBar.spec.js
@@ -11,6 +11,7 @@ describe('client > src > Body > AppListStatusBar', () => {
     );
     // check if appListStatusBar is rendered on page
     assert(driver.elementExists('.app-list-status-bar-container'), 'did not render correctly');
+    assert.equal(driver.getText('.app-list-status-bar-container'), '1 app matches your search', 'did not display the correct message');
   });
 
   it('Displays no apps if appCount is not passed in', () => {
@@ -23,5 +24,12 @@ describe('client > src > Body > AppListStatusBar', () => {
       text.includes('There are no matching apps. Please broaden your search criteria'),
       'did not display no matching apps message when appCount is 0'
     );
+  });
+
+  it('Displays the correct message with 7 apps', () => {
+    const driver = new Driver(
+      <AppListStatusBar appCount={7} />
+    );
+    assert.equal(driver.getText('.app-list-status-bar-container'), '7 apps match your search', 'did not display the correct message');
   });
 });
