2eb909ab0f2517c29893a97504e4f7e9c56fadc8
Shihan Li <lshhenry98@gmail.com>
2019-08-03 14:58:28 -0400

    added shell for okay button

--- client/src/shared/OkayButton.js
+++ client/src/shared/OkayButton.js
@@ -1,44 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class CopyButton extends Component {
-  // copy the prop passed in to the clipboard
-  copyText() {
-    const { text } = this.props;
-    // Create new element
-    const elem = document.createElement('textarea');
-    // Set its value to the text that we want copied
-    elem.value = text;
-    // Set non-editable to avoid focus and move outside of view
-    elem.setAttribute('readonly', '');
-    elem.style = { position: 'absolute', left: '-9999px' };
-    // append it to the document
-    document.body.appendChild(elem);
-    // Select text inside element
-    elem.select();
-    // Copy text to clipboard
-    document.execCommand('copy');
-    // Remove temporary element
-    document.body.removeChild(elem);
-  }
+class OkayButton extends Component {
 
   render() {
+    const { text, onClick } = this.props;
+
     return (
       <button
         type="button"
-        className="btn-clipboard rounded border-0 bg-secondary text-white font-weight-bold"
-        id="copybutton-text"
-        onClick={() => { this.copyText(); }}
+        className="btn btn-info"
+        onClick={onClick}
       >
-          Copy
+        {text}
       </button>
     );
   }
 }
 
-CopyButton.propTypes = {
-  // the text to copy to clipboard
+OkayButton.propTypes = {
+  // the text display on the botton
   text: PropTypes.string.isRequired,
+  // the onClick function to close down the modal
+  onClick: PropTypes.func.isRequired,
 };
 
-export default CopyButton;
+export default OkayButton;
