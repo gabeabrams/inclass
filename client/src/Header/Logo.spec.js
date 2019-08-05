a5027781236104f8a8e7d4b2972b58b2e34bea40
Gabe Abrams <gabeabrams@gmail.com>
2019-08-05 09:34:05 -0400

    Updated jsx to use url constant

--- client/src/Header/Logo.spec.js
+++ client/src/Header/Logo.spec.js
@@ -7,13 +7,13 @@ import Logo from './Logo';
 
 describe('client > src > Header > Logo', () => {
   it('Displays the AppStore Logo', async () => {
+    const url = 'https://www.google.com/search?q=harvard+logo&prmd=isnv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv_rCRxsfjAhUFuVkKHf3TAKYQ_AUoAXoECBIQAQ&biw=870&bih=701&dpr=2#imgrc=DYmtqTjJwjBb2M';
     const driver = new Driver(
       <Logo
-        url="https://www.google.com/search?q=harvard+logo&prmd=isnv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv_rCRxsfjAhUFuVkKHf3TAKYQ_AUoAXoECBIQAQ&biw=870&bih=701&dpr=2#imgrc=DYmtqTjJwjBb2M"
+        url={url}
       />
     );
 
-    const url = 'https://www.google.com/search?q=harvard+logo&prmd=isnv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv_rCRxsfjAhUFuVkKHf3TAKYQ_AUoAXoECBIQAQ&biw=870&bih=701&dpr=2#imgrc=DYmtqTjJwjBb2M';
     // Make sure that the logo container is rendered
     await assert(driver.elementExists('.logo-container'), 'Logo container not rendering');
     // Make sure that logo image is rendered
