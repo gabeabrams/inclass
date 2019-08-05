a7ef92fb25748e90ee9f8a2e8c74a697dead53d8
Shihan Li <lshhenry98@gmail.com>
2019-08-04 21:40:15 -0400

    added test for messageBody

--- client/src/Modal/InstallOrUninstallModal/MessageBefore.spec.js
+++ client/src/Modal/InstallOrUninstallModal/MessageBefore.spec.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 import MessageBefore from './MessageBefore';
 
 describe('client > src > Modal > InstallOrUninstallModa > MessageBefore', () => {
-  it('Displays the Modal', async () => {
+  it('Displays the Modal', () => {
     const driver = new Driver(
       <MessageBefore
         messageBefore="please install with caution"
@@ -15,9 +15,8 @@ describe('client > src > Modal > InstallOrUninstallModa > MessageBefore', () => 
     );
     // check that modal is rendered
     assert(driver.elementExists('.modal'), 'modal is not rendered');
-    // check that title is rendered
+    // check that body is rendered
     assert(driver.elementExists('.message-body-container'), 'message body is not rendered');
-    assert.equal(driver.getText('.message-body-container'), 'please install with caution', 'message before was not rendered correctly');
     // check that children is rendered
     assert(driver.elementExists('.okay-button'), 'okay button is not rendered');
     assert(driver.elementExists('.cancel-button'), 'cancel button is not rendered');
---
+++ client/src/Modal/InstallOrUninstallModal/MessageBody.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Driver from 'dce-enzyme';
+import assert from 'assert';
+
+import MessageBody from './MessageBody';
+
+describe('client > src > Modal > InstallOrUninstallModa > MessageBody', () => {
+  it('Displays the body', async () => {
+    const driver = new Driver(
+      <MessageBody messageBody="Please install with caution" />
+    );
+    // check that body is rendered
+    assert(driver.elementExists('.message-body-container'), 'message body is not rendered');
+    assert.equal(driver.getText('.message-body-container'), 'Please install with caution', 'message before was not rendered correctly');
+  });
+});
