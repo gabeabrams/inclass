22d6509844a896bee501f45773743b13979efa0b
Shihan Li <lshhenry98@gmail.com>
2019-08-07 13:15:34 -0400

    added test for the changes

--- client/src/Modal/InstallOrUninstallModal/MessageBefore.spec.js
+++ client/src/Modal/InstallOrUninstallModal/MessageBefore.spec.js
@@ -22,4 +22,19 @@ describe('client > src > Modal > InstallOrUninstallModal > MessageBefore', () =>
     assert(driver.elementExists('.okay-button'), 'okay button is not rendered');
     assert(driver.elementExists('.cancel-button'), 'cancel button is not rendered');
   });
+
+  it('Displays the correct title for uninstall', () => {
+    const message = 'please install with caution';
+    const driver = new Driver(
+      <MessageBefore
+        message={message}
+        onClose={() => {}}
+        onClick={() => {}}
+        uninstall
+      />
+    );
+    // check title is rendered
+    assert(driver.elementExists('modal-title'), 'modal title is not rendered');
+    assert.equal(driver.getText('modal-title'), 'Please Read Before Uninstallling', 'did not display correct title when uninstlling');
+  });
 });
