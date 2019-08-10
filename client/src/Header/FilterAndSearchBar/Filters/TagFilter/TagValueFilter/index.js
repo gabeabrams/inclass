a5c912e40d7d819a11c1b4dcee29bb84a452864b
stesta03 <samantha.testa@tufts.edu>
2019-08-10 00:14:50 -0400

    added skeleton for TagValueFilter component

---
+++ client/src/Header/FilterAndSearchBar/Filters/TagFilter/TagValueFilter/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Import other components
+import TagValueFilterCheckBox from './TagValueFilterCheckBox';
+import TagValueFilterCount from './TagValueFilterCount';
+import TagValueFilterLabel from './TagValueFilterLabel';
+
+// Import styling
+
+class TagValueFilter extends Component {
+  render() {
+
+    const {
+      tags
+
+    } = this.props;
+
+    return (
+
+    );
+
+  }
+}
