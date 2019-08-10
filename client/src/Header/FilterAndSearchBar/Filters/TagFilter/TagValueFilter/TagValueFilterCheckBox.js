eee40a9242ef3d1a4197ce73dbb616d549daa35f
stesta03 <samantha.testa@tufts.edu>
2019-08-10 00:14:14 -0400

    began checkbox component

---
+++ client/src/Header/FilterAndSearchBar/Filters/TagFilter/TagValueFilter/TagValueFilterCheckBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Other components
+
+// Import css
+
+class TagValueFilterCheckBox extends Component {
+  render() {
+    
+    const {
+
+    } = this.props;
+
+    return (
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+        <label class="form-check-label" for="defaultCheck1">
+        Hi
+        </label>
+      </div>
+    );
+}
+
+export default TagValueFilterCheckBox;
