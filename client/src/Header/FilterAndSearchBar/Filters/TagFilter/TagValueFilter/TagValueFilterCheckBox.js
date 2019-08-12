7ddb830deb4e4dae70722bdb257047e4df474db1
stesta03 <samantha.testa@tufts.edu>
2019-08-12 12:43:51 -0400

    Added checkbox form and props validation

--- client/src/Header/FilterAndSearchBar/Filters/TagFilter/TagValueFilter/TagValueFilterCheckBox.js
+++ client/src/Header/FilterAndSearchBar/Filters/TagFilter/TagValueFilter/TagValueFilterCheckBox.js
@@ -7,19 +7,24 @@ import PropTypes from 'prop-types';
 
 class TagValueFilterCheckBox extends Component {
   render() {
-
     const {
-      isChecked
+      isChecked,
     } = this.props;
 
     return (
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
-        <label class="form-check-label" for="defaultCheck1">
-        Hi
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+        <label className="form-check-label" for="defaultCheck1">
+        Default checkbox
         </label>
       </div>
     );
+  }
 }
+
+TagValueFilterCheckBox.propTypes = {
+  // Whether the checkbox should be clicked or not
+  isChecked: PropTypes.bool.isRequired,
+};
 
 export default TagValueFilterCheckBox;
