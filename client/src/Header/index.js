023f878e81f4671fe518fb1a54a6ebacd35850f7
Anita Lam <anita.lam@tufts.edu>
2019-08-16 12:03:06 -0400

    Found missing semicolon in Header index

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import Logo from './Logo';
 import TitleBar from './TitleBar';
 import FilterAndSearchBar from './FilterAndSearchBar';
-import Filters from './FilterAndSearchBar/Filters'
+import Filters from './FilterAndSearchBar/Filters';
 
 // Import css
 import './style.css';
