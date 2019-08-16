b63f665d9fecf5f27edadd70ede69363c66bf9cb
Shihan Li <lshhenry98@gmail.com>
2019-08-16 17:27:04 -0400

    changed the status bar display options

--- client/src/Body/AppList/index.js
+++ client/src/Body/AppList/index.js
@@ -12,7 +12,16 @@ class AppList extends Component {
       apps,
       tagColors,
       onAppSelected,
+      filtered,
+      appCount,
     } = this.props;
+
+    // display or hide the status bar depending on if it's being filtered
+    const statusBarElement = (
+      filtered
+        ? <AppListStatusBar appCount={appCount} />
+        : null
+    );
 
     // map each app to AppItem element to render
     const appElements = Object.keys(apps).map((appId) => {
@@ -30,7 +39,7 @@ class AppList extends Component {
 
     return (
       <div className="app-list-container d-flex flex-column">
-        <AppListStatusBar appCount={Object.keys(apps).length} />
+        {statusBarElement}
         {appElements}
       </div>
     );
@@ -46,6 +55,17 @@ AppList.propTypes = {
   tagColors: PropTypes.objectOf(PropTypes.object).isRequired,
   // Function called when specific app in app list is clicked
   onAppSelected: PropTypes.func.isRequired,
+  // whether the app list is being filtered
+  filtered: PropTypes.bool,
+  // how many apps are displayed after filtering
+  appCount: PropTypes.number,
+};
+
+AppList.defaultProps = {
+  // default is set to not displaying status bar
+  filtered: false,
+  // default app count is 0
+  appCount: 0,
 };
 
 export default AppList;
