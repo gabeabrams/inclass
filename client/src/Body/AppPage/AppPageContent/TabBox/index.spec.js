f09df38d6d3533c60c6de9d76631cc700f089e9d
Anita Lam <anita.lam@tufts.edu>
2019-08-23 11:00:23 -0400

    Added a testing file for Tab Box

---
+++ client/src/Body/AppPage/AppPageContent/TabBox/index.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Driver from 'dce-enzyme';
+import assert from 'assert';
+
+import TabBox from './index';
+
+describe('client > src > Body > AppPage > AppPageContent > TabBox', () => {
+  it('Checks there is a div around children', async () => {
+    // initializes the driver with TabBox around some text
+    const driver = new Driver(
+      <TabBox>
+        <p>Fake Text</p>
+      </TabBox>
+    );
+
+    // Checks TabBox exists
+    assert(driver.elementExists('.tabbox-container'), 'Tab Box is absent');
+  });
+});
