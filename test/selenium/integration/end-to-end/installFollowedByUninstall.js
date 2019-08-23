e6a89256253e3658ec14c33e65699296bface23d
Shihan Li <lshhenry98@gmail.com>
2019-08-23 16:32:06 -0400

    added test for install and uninstall end to end

---
+++ test/selenium/integration/end-to-end/installFollowedByUninstall.js
@@ -0,0 +1,35 @@
+require('dce-selenium');
+const assert = require('assert');
+
+const { courseId } = require('../../../../config/devEnvironment');
+
+describeS.only('Client > InstallFollowedByUninstall', function () {
+  itS('installs and immediately uninstalls app', async function (driver) {
+    await driver.visit(`https://localhost:8088/courses/${courseId}`);
+    // Click "Simulate Launch"
+    await driver.click('.launch-button');
+    // Click "Authorize"
+    await driver.wait(1000);
+    if (await driver.elementExists('.authorize-button')) {
+      await driver.click('.authorize-button');
+    }
+    // Wait 2s
+    await driver.wait(500);
+    // click into app page
+    await driver.click('#samsapp-app-title');
+    await driver.wait(300);
+    // click the install button
+    await driver.click('#install-button');
+    await driver.wait(600);
+    // click okay button
+    await driver.click('.okay-button');
+    await driver.wait(200);
+    // uninstalls the app
+    await driver.click('#uninstall-button');
+    await driver.wait(600);
+    // click okay button
+    await driver.click('.okay-button');
+    await driver.wait(200);
+    assert(await driver.elementExists('#install-button'), 'app was not uninstalled');
+  });
+});
