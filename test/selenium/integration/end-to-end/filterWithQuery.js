b7065cafe82d949965bc8821a5d86db2f14881d8
stesta03 <samantha.testa@tufts.edu>
2019-08-25 12:22:17 -0400

    added end-to-end test for query filtering

---
+++ test/selenium/integration/end-to-end/filterWithQuery.js
@@ -0,0 +1,14 @@
+require('dce-selenium');
+const assert = require('assert');
+
+describeS('End-to-End > filterWithQuery', function () {
+  itS.only('Simulates a filtering process using search query', async function (driver) {
+    await driver.launchAppStore();
+    // Type into search field
+    await driver.typeInto('.form-control', 'Grade');
+    // Look for the app that was filtered for
+    const appExists = await driver.getElementByContents('GradeUp', '.app-title-h3');
+    // Make sure that the app is still on the page
+    assert(appExists, 'GradeUp app is missing after search query');
+  });
+});
